cd7e5eb5f7518f8673598484f06082b37b4cf2f6
leroy <leroy.kariuki@student.moringaschool.com>
2024-01-17 10:51:13 +0300

    initial

--- server/app.py
+++ server/app.py
@@ -1,27 +1,17 @@
-# Import necessary modules from Flask
-from flask import Flask, request, session, make_response, jsonify
+from flask import Flask, request, session, jsonify, make_response
 
-# Import jsonify explicitly from flask.json
-from flask.json import jsonify
-
-# Create a Flask app instance
 app = Flask(__name__)
-
-# Disable JSON compact formatting in the app
 app.json.compact = False
 
-# Set a secret key for session security
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
 
-# Define a route for handling session information
 @app.route('/sessions/<string:key>', methods=['GET'])
 def show_session(key):
 
-    # Initialize or retrieve session variables
     session["hello"] = session.get("hello") or "World"
+    
     session["goodnight"] = session.get("goodnight") or "Moon"
 
-    # Create a JSON response with session details and cookies
     response = make_response(jsonify({
         'session': {
             'session_key': key,
@@ -29,14 +19,12 @@ def show_session(key):
             'session_accessed': session.accessed,
         },
         'cookies': [{cookie: request.cookies[cookie]}
-                    for cookie in request.cookies],
+            for cookie in request.cookies],
     }), 200)
 
-    # Set a custom cookie named 'mouse'
     response.set_cookie('mouse', 'Cookie')
 
     return response
 
-# Run the Flask app on port 5555 if the script is executed directly
 if __name__ == '_main_':
     app.run(port=5555)
